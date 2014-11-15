4b82980f2c64ecd111aaf5fd7affbb536be23a48
Sun-Yi Lin <sl3833@columbia.edu>
2014-11-15 13:28:55 -0500

    change the way to iterate the pgd

--- flo-kernel/mm/expopt.c
+++ flo-kernel/mm/expopt.c
@@ -10,6 +10,7 @@
 #include <linux/mm_types.h>
 #include <linux/mman.h>
 
+
 SYSCALL_DEFINE3(expose_page_table, pid_t, pid,
 		unsigned long, fake_pgd,
 		unsigned long, addr)
@@ -19,6 +20,28 @@ SYSCALL_DEFINE3(expose_page_table, pid_t, pid,
 	if ((int)pid == -1)
 		pid = current->pid;
 
+	int iter;
+	int pgd_num = (PTRS_PER_PGD / 4) * 3;
+	pgd_t *pgd_crnt;
+
+	struct task_struct *p = pid_task(find_vpid(pid), PIDTYPE_PID);
+	struct mm_struct *mm = p->mm;
+
+	pgd_crnt = mm->pgd;
+	for (iter = 0; iter < pgd_num; iter++) {
+		printk("Index: %d(0x%lx)   --->\tpte[0]: 0x%lx(%d), pte[1]: 0x%lx(%d)\n",
+				iter,
+				pgd_crnt,
+				(*pgd_crnt)[0],
+				pte_pfn((*pgd_crnt)[0]),
+				(*pgd_crnt)[1],
+				pte_pfn((*pgd_crnt)[1]));
+		pgd_crnt++;
+	}
+	printk("Number of pgd: %d\n", pgd_num);
+
+
+/*
 	struct task_struct *p = pid_task(find_vpid(pid), PIDTYPE_PID);
 	struct mm_struct *mm = p->mm;
 	struct vm_area_struct *vma = mm->mmap;
@@ -33,23 +56,25 @@ SYSCALL_DEFINE3(expose_page_table, pid_t, pid,
 		printk("start = 0x%lx\n", start);
 		for (it = start; it < end; it += 0x1000) {
 			
+
 			int index_pgd = pgd_index(it);
 			int index_pte = pte_index(it);
 			printk("pgd_index(0x%lx) = %d\n", it, index_pgd);
 			printk("pte_index = %d\n", index_pte);
 			pgd = pgd_offset(mm, it);
 
-			/* (2^10)*sizeof(unsigned long) = each PTE size
-			(2^9) = number of PTES in a page table*/	
+			//(2^10)*sizeof(unsigned long) = each PTE size
+			//(2^9) = number of PTES in a page table	
 			remap_pfn_range(find_vma(mm, addr),
 					addr + index_pgd*((2^9)*(2^10)*sizeof(unsigned long)) + index_pte*(2^10)*sizeof(unsigned long),
 					*pgd + index_pte*(2^10)*sizeof(unsigned long),
 					(2^10)*sizeof(unsigned long),
 					PROT_READ);
+
 		}
 		printk("end = 0x%lx\n", end);
 		vma = vma->vm_next;
 	} while (vma);
-
+*/
 	return 0;	
 }
