eec2fcaed4336296b7f029fd21f0238ed5dada45
Sun-Yi Lin <sl3833@columbia.edu>
2014-11-19 15:26:25 -0500

    adjust naming

--- flo-kernel/mm/expopt.c
+++ flo-kernel/mm/expopt.c
@@ -17,14 +17,14 @@ SYSCALL_DEFINE3(expose_page_table, pid_t, pid,
 		unsigned long, fake_pgd,
 		unsigned long, addr)
 {
-	if (addr == NULL || fake_pgd == NULL)
+	if ((unsigned long *)addr == NULL || (unsigned long *)fake_pgd == NULL)
 		return -EINVAL;
 	if ((int)pid == -1)
 		pid = current->pid;
 
 	int iter, pgd_num;
 	pgd_t *pgd_crnt;
-	unsigned long pte_base, *fake_pgd_k, *fake_pgd_k_temp;
+	unsigned long L2T_base, *fake_pgd_k, *fake_pgd_k_iter;
 	unsigned long pte_0_base, pte_1_base;
 	struct task_struct *p = pid_task(find_vpid(pid), PIDTYPE_PID);
 	struct mm_struct *mm = p->mm;
@@ -34,44 +34,44 @@ SYSCALL_DEFINE3(expose_page_table, pid_t, pid,
 	if (!fake_pgd_k)
 		return -EFAULT;
 
-	fake_pgd_k_temp = fake_pgd_k;
+	fake_pgd_k_iter = fake_pgd_k;
 	pgd_crnt = mm->pgd;
 	for (iter = 0; iter < USER_PTRS_PER_PGD; iter++) {
-		pte_base = ((unsigned long)((*pgd_crnt)[0])) & 0xfffff000;
+		L2T_base = ((unsigned long)((*pgd_crnt)[0])) & 0xfffff000;
 		pte_0_base = (*pgd_crnt)[0];
 		pte_1_base = (*pgd_crnt)[1];
-/*
-		printk("(%d)L1 Tbl Ptr: 0x%08lx ---> L2 Table base: 0x%08lx\n",
-				iter, pgd_crnt, pte_base);
-		printk("\tL2 H/W Tbl Base Pointer[0] = 0x%08lx\n", pte_0_base);
-		printk("\tL2 H/W Tbl Base Pointer[1] = 0x%08lx\n", pte_1_base);
-*/
+
+		pr_debug("(%d)L1 Tbl Ptr: 0x%08lx ---> L2 Table base: 0x%08lx\n",
+				iter, (unsigned long)pgd_crnt, L2T_base);
+		pr_debug("\tL2 H/W Tbl Base Ptr[0] = 0x%08lx\n", pte_0_base);
+		pr_debug("\tL2 H/W Tbl Base Ptr[1] = 0x%08lx\n", pte_1_base);
+
 		pgd_crnt++;
 		down_write(&(current->mm->mmap_sem));
-		if (pte_base != 0x00000000) {
+		if (L2T_base != 0x00000000) {
 			if (access_ok(VERIFY_WRITE, addr, PAGE_SIZE)) {
 				remap_pfn_range(find_vma(current->mm, addr),
 					addr,
-					pte_base >> PAGE_SHIFT,
+					L2T_base >> PAGE_SHIFT,
 					PAGE_SIZE,
 					current->mm->mmap->vm_page_prot);
-				*fake_pgd_k = addr;
+				*fake_pgd_k_iter = addr;
 			} else
 				return -EFAULT;
 			pr_debug("Remap L2 Table: %d\n", iter);
 		} else
-			pr_debug("Skip L1 Table: %d\n", iter);
+			pr_debug("Skip  L1 Table: %d\n", iter);
 		up_write(&(current->mm->mmap_sem));
 
 		if (iter != pgd_num - 1)
-			fake_pgd_k++;
+			fake_pgd_k_iter++;
 		addr += PAGE_SIZE;
 	}
-	/*printk("Number of pgd: %d\n", pgd_num);*/
+
 	if (copy_to_user((unsigned long *)fake_pgd,
-				fake_pgd_k_temp,
+				fake_pgd_k,
 				sizeof(unsigned long) * 4096))
 		return -EFAULT;
-	kfree(fake_pgd_k_temp);
+	kfree(fake_pgd_k);
 	return 0;
 }
